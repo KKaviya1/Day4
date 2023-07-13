f12bab493a033c16646750280922118a2230d690
KKaviya1 <139097458+KKaviya1@users.noreply.github.com>
2023-07-13 14:49:47 +0530

    Add files via upload

---
+++ Assignment/Assignment - 4/Assgn-P1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Assgn_P1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+                Console.WriteLine("Enter a number: ");
+                int number = int.Parse(Console.ReadLine());
+
+                // Check if the number is positive, negative, or zero
+                if (number > 0)
+                {
+                    Console.WriteLine("The number is positive.");
+                }
+                else if (number < 0)
+                {
+                    Console.WriteLine("The number is negative.");
+                }
+                else
+                {
+                    Console.WriteLine("The number is zero.");
+                }
+
+                // Check if the number is even or odd using the conditional operator
+                string parity = (number % 2 == 0) ? "even" : "odd";
+                Console.WriteLine("The number is " + parity + ".");
+            }
+        }
+ }
+
---
+++ Assignment/Assignment - 5/Assignment - 5/Program.cs
@@ -0,0 +1,48 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment___5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+                int attempts = 3;
+                string validUsername = "viya";
+                string validPassword = "kav346";
+
+                while (attempts > 0)
+                {
+                    Console.WriteLine("Enter username: ");
+                    string username = Console.ReadLine();
+
+                    Console.WriteLine("Enter password: ");
+                    string password = Console.ReadLine();
+
+                    if (username == validUsername && password == validPassword)
+                    {
+                        Console.WriteLine("Login successful.");
+                        return; // Exit the program
+                    }
+                    else if (username == validUsername && password != validPassword)
+                    {
+                        Console.WriteLine("Invalid password.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid username.");
+                    }
+
+                    attempts--;
+                    Console.WriteLine("Attempts remaining: " + attempts);
+                }
+
+                Console.WriteLine("Login failed.");
+            }
+        }
+
+    }
